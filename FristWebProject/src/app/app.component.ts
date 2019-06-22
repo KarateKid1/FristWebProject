0a62a46071214996992f41469841d07606123c73
Ale <avramescu.alexandra@gmai.com>
2019-06-22 13:28:00 +0300

    idk

--- FristWebProject/src/app/app.component.ts
+++ FristWebProject/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FristWebProject';
+  person1: Number;
+
+    addPerson1() {
+        this.person1=10;
+        console.log(this.person1);
+    } 
+  person2: Number;
+
+    addPerson2() {
+        this.person2=20;
+        console.log(this.person2);
+    } 
 }
+
